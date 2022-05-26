2af3cb65d41928e64d8c17f2286931df5ea1c1f0
shimron <shimron.d@ceylonsolutions.com>
2022-05-26 17:06:57 +0530

    check git

--- sample_api/Controllers/WeatherForecastController.cs
+++ sample_api/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        // check git
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
